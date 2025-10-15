c7e795e33c529546f257afda95af976f88c9dd1c
grassedprogrammer <the2ndalienboy802@gmail.com>
2025-10-16 06:52:32 +0800

    added general_typedefs.h

---
+++ asimpleyeast/casimplebred/General_typedefs/general_typedefs.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#ifndef GENERAL_TYPEDEFS_CASIMPLEBRED
+#define GENERAL_TYPEDEFS_CASIMPLEBRED
+typedef unsigned char uchar;
+typedef uint8_t stdbyte;
+typedef unsigned short ushrt;
+typedef unsigned short ushort;
+//my code is roasting me now!>:
+#endif
